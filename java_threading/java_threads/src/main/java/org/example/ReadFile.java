68d4cb6323143729888cf91407a6b28cf37c3770
Maryiamreda <maryiamreda@gmail.com>
2025-08-10 21:07:54 +0300

    .

---
+++ java_threading/java_threads/src/main/java/org/example/ReadFile.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+interface Read {
+    ArrayList<String> readFile(int fileIndex) throws FileNotFoundException;
+}
+
+class ReadFile implements Read {
+    private ArrayList<String> names = new ArrayList<>();
+    public ArrayList<String> getNames() {
+        return names;
+    }
+    public ArrayList<String> readFile(final int fileIndex) throws FileNotFoundException {
+        File myObj = new File("F:\\" + fileIndex + ".txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            names.add(data);
+        }
+        myReader.close();
+        return new ArrayList<>(names); // return a copy
+    }
+
+
+}
--- java_threading/java_threads/src/main/java/org/example/ReadFiles.java
+++ java_threading/java_threads/src/main/java/org/example/ReadFiles.java
@@ -1,46 +1,36 @@
 package org.example;
-import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
 public class ReadFiles {
+    public static void main(String[] args) throws InterruptedException {
+        ReadFile reader = new ReadFile();
 
-public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);  //create thread pool
 
-        ExecutorService executorService = Executors.newFixedThreadPool(5); //create thread pool
-ArrayList<String> names=new ArrayList<String>();
         for (int i = 0; i < 5; i++) {
             final int fileIndex = i;
-            executorService.execute( ()-> {
-try{
-    readFile(fileIndex);
-}catch(FileNotFoundException e){
-    System.out.println("The file does not exist.");
-}
+            executorService.execute(() -> {
+                try {
+                    reader.readFile(fileIndex);
+                } catch (FileNotFoundException e) {
+                    System.out.println("The file does not exist ");
+                }
             });
         }
 
-executorService.awaitTermination(5, TimeUnit.SECONDS);  //maximum wait time of 5 seconds.
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
 
-executorService.close();
 
+        ArrayList<String> names = reader.getNames();
+        Collections.sort(names);
+
+        System.out.println(names);
     }
-    public static ArrayList<String>  readFile(final int fileIndex) throws FileNotFoundException {
-        File myObj = new File("F:\\"+fileIndex+".txt");
-ArrayList<String>lines=new ArrayList<>();
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                lines.add(data);
-                System.out.println(data);
-            }
-        myReader.close();
-            return lines;
-
-
-
-    }
-    }
+}
